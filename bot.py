ad2e62de5088bea5a02c5c64f59c216403254dab
Balázs Kerekes <kerekes.balazs.12@gmail.com>
2026-05-22 08:54:58 +0200

    fix bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
