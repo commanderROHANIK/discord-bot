7e0799a79a64c51b5a2e85471aeae0df9963df96
Balázs Kerekes <kerekes.balazs.12@gmail.com>
2026-05-26 20:29:23 +0200

    add dotenv

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 import json
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
